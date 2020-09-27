cc1656105ca45a36b363d55b49e9a8cb9f3e105a
Kerri Sexton <k.sexton804@gmail.com>
2020-09-27 16:51:07 -0400

    trying to insert buttons dynamically

--- script.js
+++ script.js
@@ -33,33 +33,40 @@ THEN I can save my initials and score*/
 //Prompt to enter initials
 //Save score and initials in local storage
 
+//Vars for pages
 var titlePage = document.querySelector('#title-page');
 var questionPage = document.querySelector('#question-page');
 var resultsPage = document.querySelector('#result-page');
 var hsPage = document.querySelector('#highscore-page');
 var startBtn = document.querySelector("#start");
 
+//Vars for elements on pages
+var questionPos = document.querySelector('#question-class');
+var initialQues = 0;
+
 const myQuestions = [
     {
-      question: "Question 1",
+      question: "Question 1?",
       answers: {
         a: "one",
         b: "two",
-        c: "three"
+        c: "three",
+        d: "four"
       },
       correctAnswer: "c"
     },
     {
-      question: "Question 2",
+      question: "Question 2?",
       answers: {
         a: "one",
         b: "two",
-        c: "three"
+        c: "three",
+        d: "four"
       },
       correctAnswer: "c"
     },
     {
-      question: "Question 3",
+      question: "Question 3?",
       answers: {
         a: "one",
         b: "two",
@@ -74,31 +81,79 @@ const myQuestions = [
 
     console.log('Function question iterator is running.');
     
-    //Removes the start button
+    //Hides title page
+    titlePage.classList.add('hidden');
 
-    var elem = document.querySelector("#start");
-    elem.parentNode.removeChild(elem);
+    //Unhides question page
+    questionPage.classList.remove('hidden');
 
-    //For each loop to go through all the questions
-
-    // myQuestions.forEach(myFunction);
-
-    // function myFunction(item, index){
-    //     console.log('Inside the forEach loop: ', item, index);
-    //     document.getElementById('card-body').innerHTML;
-    // }
-
+    //Add each question to the page
     for(var i = 0; i < myQuestions.length; i++){
 
-       var q = myQuestions[i];
-       var newEl = document.createElement('#card-header');
-       questionsHere.appendChild(q.question);
-       console.log('Question is: '. q.question); 
+        var q = myQuestions[i];
+        var questionPos = document.querySelector('#question-class');
 
+      questionPos.append(q.question);
+    //   console.log('Question is: '. q.question); 
+
+      var answerPos =document.querySelector('#answer1');
+      answerPos.append(q.answer1);
+    //   console.log('Question is: '. q.question); 
     }
 
-    return;
+
   }
+
+//   function rightOrWrong(){
+
+//   }
+
+// function endQuiz(){
+
+// }
 
   startBtn.addEventListener("click", questionIterator);
 
+// function questionIterator() {
+   //var q = myQuestions[initialQues];
+//}
+
+//loop over choices
+// ereate a button for each choice
+   //var userChoices = document.createElement("button")
+
+// add an onclick event listener to each choice
+     // userChoices.onClick = isRightOrWrong 
+// display on page
+
+//vv a seperate function from questionIterator
+//seperate end quiz function
+
+//isRightOrWrong
+    //check if user is wrong or right
+         // if (this !== myQuestions[initialQues].correctAnswer) {
+             // time penalty
+                 // time -= 5
+         //}
+    // move to next Q
+         // initialQues++;
+    // do you have more questions?
+        // if(initalQues === myQuestions.length) {
+            //endQuiz()
+        
+    //} else {
+        //questionIterator()
+    //}
+
+//hishScores
+    //get value of input
+    //get  saved scores or, if no saved scores, set to an empty array
+    //make an object to score user data
+    // save to localstorage
+
+//make buttons to populate answers    
+    //var initialQuestion = myQuestions[initialQues]
+    // initialQuestion.answers.forEach(function(i) {
+        //create button
+        //userChoices.textContent = i + answers
+    //})
